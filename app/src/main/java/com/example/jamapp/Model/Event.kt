36af61a9c54d735c6b137502967108af3d25ee47
Johann <mast3r1sgr3at@hotmail.com>
2019-12-29 09:29:33 +0800

    View event info complete

--- app/src/main/java/com/example/jamapp/Model/Event.kt
+++ app/src/main/java/com/example/jamapp/Model/Event.kt
@@ -1,5 +1,6 @@
 package com.example.jamapp.Model
 
 import java.io.Serializable
+import java.util.*
 
-data class Event(val host_id : String = "", val title : String = "", val address : String = "", val description : String = "", val imageLink : String = "", val learnMoreLink : String = "") : Serializable
+data class Event(val host_id : String = "", val title : String = "", val address : String = "", val description : String = "", val imageLink : String = "", val learnMoreLink : String = "", val date : String = "") : Serializable
--- app/src/main/java/com/example/jamapp/create_event.kt
+++ app/src/main/java/com/example/jamapp/create_event.kt
@@ -1,14 +1,18 @@
 package com.example.jamapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.DatePicker
 import android.widget.Toast
 import com.example.jamapp.Model.Event
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_create_event.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class create_event : AppCompatActivity() {
 
@@ -22,6 +26,32 @@ class create_event : AppCompatActivity() {
 
         // Initialize db
         auth = FirebaseAuth.getInstance()
+
+        // Create calendar to store event date
+        val cal : Calendar = Calendar.getInstance()
+
+        // Set date
+        // Reference : https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/
+        val datePicker = object : DatePickerDialog.OnDateSetListener{
+            // Triggered when date is set
+            override fun onDateSet(view : DatePicker, year : Int, month: Int, day: Int){
+
+                // Set the selected date to calendar
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_YEAR, day)
+
+                // Update date in EditText view
+                val dateFormat = "dd/MM/yyyy"
+                val sdf = SimpleDateFormat(dateFormat, Locale.UK)
+                createEventDate!!.setText(sdf.format(cal.time))
+            }
+        }
+
+        createEventDate.setOnClickListener{
+            // Creates the date picker dialog when user clicks on the EditText field
+            val datePickerDialog = DatePickerDialog(this, datePicker, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
 
     public fun backToMyEvents(view : View){
@@ -29,9 +59,14 @@ class create_event : AppCompatActivity() {
     }
 
     public fun createEvent(view : View){
+        // Convert the string to calendar
+        val cal = Calendar.getInstance() as Calendar
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        cal.time = sdf.parse(createEventDate.text.toString())
+
 
         // Create event object
-        var newEvent = Event( host_id = auth.uid.toString(), title = createEventTitle.text.toString(), address = createEventVenue.text.toString(), description = createEventDescription.text.toString(), imageLink = createEventImageUrl.text.toString(), learnMoreLink = createEventWebLink.text.toString())
+        var newEvent = Event( host_id = auth.uid.toString(), title = createEventTitle.text.toString(), address = createEventVenue.text.toString(), description = createEventDescription.text.toString(), imageLink = createEventImageUrl.text.toString(), learnMoreLink = createEventWebLink.text.toString(), date = createEventDate.text.toString())
         dbRef.child("event").push().setValue(newEvent).addOnSuccessListener {
             // Show success message
             val toast = Toast.makeText(applicationContext, "event_fragment created successfully", Toast.LENGTH_SHORT)
